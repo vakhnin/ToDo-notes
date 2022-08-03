856b4b0b8312d87864ce4e46a121ed2b25928431
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-03 12:07:10 +0300

    Add test users by fill_db command

--- users/management/commands/fill_db.py
+++ users/management/commands/fill_db.py
@@ -6,5 +6,30 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='geekbrains'):
             User.objects.create_superuser(username='geekbrains',
+                                          first_name='geekbrains_first_name',
+                                          last_name='geekbrains_last_name',
                                           email='geekbrains@geekshop.local',
+                                          password='geekbrains')
+
+        if not User.objects.filter(username='test1'):
+            User.objects.create_superuser(username='test1',
+                                          first_name='first_name1',
+                                          last_name='last_name1',
+                                          email='test1@geekshop.local',
+                                          password='geekbrains')
+
+
+        if not User.objects.filter(username='test2'):
+            User.objects.create_superuser(username='test2',
+                                          first_name='first_name2',
+                                          last_name='last_name2',
+                                          email='test2@geekshop.local',
+                                          password='geekbrains')
+
+
+        if not User.objects.filter(username='test3'):
+            User.objects.create_superuser(username='test3',
+                                          first_name='first_name3',
+                                          last_name='last_name3',
+                                          email='test3@geekshop.local',
                                           password='geekbrains')
