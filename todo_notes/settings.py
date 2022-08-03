c14761767c6a948a6d12935383003cb175e65a94
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-03 11:07:50 +0300

    Start users app

--- todo_notes/settings.py
+++ todo_notes/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
+AUTH_USER_MODEL = 'users.User'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # TODO-notes project
+    'users',
 ]
 
 MIDDLEWARE = [
