4e4c9e0290ca4284722808e2b6b002d10fa05221
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-03 11:40:08 +0300

    Made base API for Users

--- todo_notes/settings.py
+++ todo_notes/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-y5jko_yyt9t(r_z8a*&f3sz366#9uv=!d)22#$v3ufxbw^mah&
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 AUTH_USER_MODEL = 'users.User'
@@ -41,6 +39,7 @@ INSTALLED_APPS = [
 
     # TODO-notes project
     'users',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -73,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todo_notes.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -83,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -103,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -116,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
