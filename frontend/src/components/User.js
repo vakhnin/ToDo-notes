edc31ba513c4eae074b15332c2e3aad56a719f2d
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-07 14:32:12 +0300

    Show more info about users

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -3,12 +3,10 @@ import React from 'react'
 const UserItem = ({ user }) => {
     return (
         <tr>
-            <td>
-                {user.first_name}
-            </td>
-            <td>
-                {user.last_name}
-            </td>
+            <td>{user.username}</td>
+            <td>{user.first_name}</td>
+            <td>{user.last_name}</td>
+            <td>{user.email}</td>
         </tr>
     )
 }
@@ -18,16 +16,14 @@ const UserList = ({ users }) => {
         <table>
             <thead>
                 <tr>
-                    <th>
-                        First name
-                    </th>
-                    <th>
-                        Last Name
-                    </th>
+                    <th>Username</th>
+                    <th>First name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
                 </tr>
             </thead>
             <tbody>
-                {users.map((user) => <UserItem  key={user.username} user={user} />)}
+                {users.map((user) => <UserItem key={user.username} user={user} />)}
             </tbody>
         </table>
     )
