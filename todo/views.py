420244e2e9cc882dd0992ce86d58559b04846b7d
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-09 11:41:47 +0300

    Add ToDo model API

--- todo/views.py
+++ todo/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.viewsets import ModelViewSet
 
-from todo.models import Project
-from todo.serializers import ProjectModelSerializer
+from todo.models import Project, ToDo
+from todo.serializers import ProjectModelSerializer, ToDoModelSerializer
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+
+
+class ToDoModelViewSet(ModelViewSet):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoModelSerializer
