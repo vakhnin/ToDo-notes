b6d09b108d4175b14eb8675f23699666c0757d90
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-13 11:05:47 +0300

    Set operations for User: view list, view detail, change

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
@@ -8,11 +9,17 @@ from users.models import User
 from users.serializers import UserModelSerializer
 
 
-class UserLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 2
+# class UserLimitOffsetPagination(LimitOffsetPagination):
+#     default_limit = 2
+#
+#
+# class UserModelViewSet(ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserModelSerializer
+#     pagination_class = UserLimitOffsetPagination
 
 
-class UserModelViewSet(ModelViewSet):
-    queryset = User.objects.all()
+class UserModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin, viewsets.GenericViewSet):
     serializer_class = UserModelSerializer
-    pagination_class = UserLimitOffsetPagination
+    queryset = User.objects.all()
