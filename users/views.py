a252abd2b02f5a205dcfe4f5f1fa584bcfceed56
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-13 10:41:41 +0300

    Set size of page = 2 for User list

--- users/views.py
+++ users/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import User
 from users.serializers import UserModelSerializer
 
 
+class UserLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 2
+
+
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    pagination_class = UserLimitOffsetPagination
