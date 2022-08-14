92341ffe079ca6051d4a2cddaa013d45ae78541f
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-14 10:11:44 +0300

    Redefinition destroy of ToDo record

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ViewSet
 
 from todo.filters import ProjectNameFilter, TodoFilter
 from todo.models import Project, ToDo
@@ -11,6 +14,7 @@ from todo.serializers import ProjectModelSerializer, ToDoModelSerializer
 
 class ProjectLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 10
+
 
 class ToDoLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 20
@@ -28,3 +32,13 @@ class ToDoModelViewSet(ModelViewSet):
     serializer_class = ToDoModelSerializer
     pagination_class = ToDoLimitOffsetPagination
     filterset_class = TodoFilter
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            instance.is_active = False
+            instance.save()
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(status=status.HTTP_200_OK)
