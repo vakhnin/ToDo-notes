52c387f0d119f095000d322f20aa981aa4899a85
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-18 10:31:56 +0300

    Made route for main, users, projects, ToDos pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 import './App.css';
 import UserList from './components/User.js';
 import Footer from './components/Footer.js';
-import Menu from './components/Menu.js';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
 
 class App extends React.Component {
   constructor(props) {
@@ -26,13 +27,39 @@ class App extends React.Component {
       }).catch(error => console.log(error))
   }
 
-
   render() {
     return (
-      <div>
-        <Menu />
+      <div className="App">
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Главная</Link>
+              </li>
+              <li>
+                <Link to='/users'>Пользователи</Link>
+              </li>
+              <li>
+                <Link to='/projects'>Проекты</Link>
+              </li>
+              <li>
+                <Link to='/todos'>ToDos</Link>
+              </li>
+            </ul>
+          </nav>
+          <hr />
+          <Routes>
+            <Route path='/' element={<h2>Главная</h2>} />
+            <Route path='/users' element={
+              <div>
+                <h2>Пользователи</h2>
+                <UserList users={this.state.users} />
+              </div>} />
+            <Route path='/projects' element={<h2>Проекты</h2>} />
+            <Route path='/todos' element={<h2>ToDos</h2>} />
+          </Routes>
+        </Router>
         <hr />
-        <UserList users={this.state.users} />
         <Footer />
       </div>
     )
