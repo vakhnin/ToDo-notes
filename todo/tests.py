5937c3479d35b5c130d3c612dfa43ae3ddfead8d
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-27 09:52:24 +0300

    Test using APIClient

--- todo/tests.py
+++ todo/tests.py
@@ -1,24 +1,28 @@
 from django.test import TestCase
+from mixer.backend.django import mixer
 
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, APIClient
 
+from users.models import User
 from .models import Project
 from .views import ProjectModelViewSet
 
 
 # Create your tests here.
 class TestProjectViewSet(TestCase):
+    def setUp(self) -> None:
+        self.url = '/api/projects/'
+
     def test_get_project_list(self):
         factory = APIRequestFactory()
-        request = factory.get('/api/projects/')
+        request = factory.get(self.url)
         view = ProjectModelViewSet.as_view({'get': 'list'})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_get_detail(self):
-    #     project = Project.objects.create(name='Пушкин', birthday_year=1799)
-    #     client = APIClient()
-    #     response = client.get(f'/api/projects/{project.id}/')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
+    def test_get_project_detail(self):
+        project = mixer.blend(Project)
+        client = APIClient()
+        response = client.get(f'{self.url}{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
