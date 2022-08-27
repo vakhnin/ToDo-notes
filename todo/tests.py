36511948ccbe49975c55b555ed8544df24c07d0d
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-08-27 10:04:39 +0300

    Test using APITestCase

--- todo/tests.py
+++ todo/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from mixer.backend.django import mixer
 
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, APIClient
+from rest_framework.test import APIRequestFactory, APIClient, APITestCase
 
 from users.models import User
 from .models import Project
@@ -25,4 +25,11 @@ class TestProjectViewSet(TestCase):
         project = mixer.blend(Project)
         client = APIClient()
         response = client.get(f'{self.url}{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+class TestProjectAPITestCase(APITestCase):
+    def setUp(self) -> None:
+        self.url = '/api/projects/'
+
+    def test_get_project_list_test_case(self):
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
