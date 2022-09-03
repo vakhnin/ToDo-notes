8450fb90920374ec9082880d3285d898cba5a6cf
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-09-03 10:43:14 +0300

    Made schema for take projects data

--- todo_notes/schema.py
+++ todo_notes/schema.py
@@ -1,8 +1,28 @@
 import graphene
+from graphene_django import DjangoObjectType
+
+from todo.models import Project
+from users.models import User
+
+
+class ProjectType(DjangoObjectType):
+    class Meta:
+        model = Project
+
+    fields = '__all__'
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+
+    fields = '__all__'
 
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    all_projects = graphene.List(ProjectType)
+
+    def resolve_all_projects(root, info):
+        return Project.objects.all()
 
 
 schema = graphene.Schema(query=Query)
