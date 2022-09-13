8e97b9a29cca29c64848559b41b2707cf3808d74
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-09-13 13:15:39 +0300

    Change DB to postgresql

--- todo_notes/settings.py
+++ todo_notes/settings.py
@@ -86,9 +86,17 @@ WSGI_APPLICATION = 'todo_notes.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'PASSWORD': 'geekbrains',
+        'USER': 'geekbrains',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
