c1a5b6eb9a04ee4342a2001940772a20293c8edb
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-09-14 12:00:10 +0300

    Make nginx frontend by Docker

--- todo_notes/settings.py
+++ todo_notes/settings.py
@@ -142,9 +142,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://0.0.0.0:80',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
