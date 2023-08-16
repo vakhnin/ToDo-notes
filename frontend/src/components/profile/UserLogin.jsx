7b7f5d0107470fc3481931020095a0844dd356bc
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2023-08-16 10:35:27 +0300

    Add login form validation

--- frontend/src/components/profile/UserLogin.jsx
+++ frontend/src/components/profile/UserLogin.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
 
 import { RESTAPI } from '../Settings'
 import Modal from 'react-bootstrap/Modal'
@@ -7,9 +8,8 @@ import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
 export default function loginModal (props) {
-  const [error, setError] = useState('')
-
-  const { register, handleSubmit, reset } = useForm({
+  const [serverError, setServerError] = useState('')
+  const { register, handleSubmit, reset, formState: { errors } } = useForm({
     defaultValues: {
       username: '',
       password: ''
@@ -23,30 +23,38 @@ export default function loginModal (props) {
     })
       .then(response => {
         props.setToken(response.data.token)
-        setError('')
         props.setModalLoginShow(false)
       }).catch(error => {
-        setError('Неверный логин или пароль')
-        return error
+        if (error.response.status === 400) {
+          setServerError('Неверные логин или пароль')
+        } else {
+          setServerError('Неизвестная ошибка сервера. Обратитесь к разработчику')
+        }
       })
   }
 
   return (
     <Modal show={props.show} onHide={() => props.setModalLoginShow(false)}
-      onShow={() => { setError(); reset() }}>
+      onShow={() => { setServerError(''); reset() }}>
       <Modal.Header closeButton>
         <Modal.Title>Авторизация</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div className='text-danger'>{error}</div>
+        <div className='text-danger'>{serverError}</div>
         <Form>
           <Form.Group className="mb-3" controlId="loginForm.ControlInput1">
             <Form.Label>Имя пользователя</Form.Label>
-            <Form.Control type="text" placeholder="Имя пользователя" {...register('username')} />
+            <Form.Control type="text" placeholder="Имя пользователя"
+              {...register('username', { required: 'Поле "Имя пользователя" не может быть пустым' })} />
+            <ErrorMessage errors={errors} name="username"
+              render={({ message }) => <div className='text-danger'>{message}</div>} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="loginForm.ControlInput2">
             <Form.Label>Пароль</Form.Label>
-            <Form.Control type="password" placeholder="Пароль" {...register('password')} />
+            <Form.Control type="password" placeholder="Пароль"
+              {...register('password', { required: 'Поле "Пароль" не может быть пустым' })} />
+            <ErrorMessage errors={errors} name="password"
+              render={({ message }) => <div className='text-danger'>{message}</div>} />
           </Form.Group>
         </Form>
       </Modal.Body>
