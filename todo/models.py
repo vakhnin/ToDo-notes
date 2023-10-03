6026f60c2ec06ea43d499af523272131bff67c6e
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2023-10-03 09:55:27 +0300

    Change unique ToDo model

--- todo/models.py
+++ todo/models.py
@@ -18,13 +18,16 @@ class Project(models.Model):
 
 class ToDo(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    name = models.CharField(max_length=32, unique=True)
+    name = models.CharField(max_length=32)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     creator = models.ForeignKey(User, related_name='creator_todos',
                                 on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('project', 'name',)
 
     def __str__(self):
         return self.text
